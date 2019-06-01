56f428c062f088ba91f2dc3c992c990900f0c01c
James Harrison <james@fasttortoise.co.uk>
2019-06-02 00:14:49 +0100

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,88 @@
+<template>
+  <div id="app">
+    <routerView/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+body {
+  background: url("/assets/img/home.jpg");
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  font-family: 'Roboto', sans-serif !important;
+}
+
+footer {
+  color: white;
+  padding: 2vh 2vw;
+  padding-bottom: 0;
+  background: #1b1b1b;
+}
+
+/* CONTENT */
+.div-mid {
+  top: 45%;
+  transform: translateY(-50%);
+  position: relative;
+}
+
+.content {
+  width: 80%;
+  min-height: 65vh;
+  margin: 4vh auto;
+  padding: 5vh 2vw;
+  background: #f7f7f7;
+  box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.5);
+}
+
+@media(max-width: 992px) {
+  .content {
+    padding: 5vh 5vw;
+  }
+}
+
+/* NAVBAR */
+nav {
+  background-color: #0275d8 !important;
+}
+
+.nav-item {
+  padding: 0 1vw;
+  position: relative;
+}
+
+/* BLOG */
+.blogpage {
+  width: 80%;
+  margin: 0 auto;
+}
+
+.blogpost {
+  margin: 4vh auto;
+  padding: 5vh 2vw;
+  background: #f7f7f7;
+  box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.5);
+}
+
+/* Text */
+.text-discord {
+  color: #7289da;
+}
+
+.btn-discord {
+    color: #fff;
+    background-color: #7289da
+}
+
+.btn-discord:hover,.btn-discord:focus,.btn-discord:active,.btn-discord:active:focus {
+    color: #fff;
+    background-color: #667bc4
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,52 @@
+<template>
+  <main class="text-white">
+    <div class="div-mid text-center">
+      <img class="homelogo" src="/assets/img/logo/logo.png"/>
+      <h3>Come and PLACEHOLDER on <kbd class="pl-3">Play.CapeCraft.Net <i class="fa fa-copy" data-clipboard-text="Play.CapeCraft.Net"></i></kbd></h3>
+      <a class="btn btn-discord btn-raised" target="_blank" href="/discord"><i class="fab fa-discord"></i> Join our Discord</a>
+    </div>
+  </main>
+</template>
+
+<script>
+  export default {
+    name: 'Home'
+  }
+</script>
+
+<style>
+main {
+  height: 100vh;
+}
+
+.nav-item {
+  padding: 0 1vw
+}
+
+body > .nav, body > .primary-color {
+  position: absolute;
+  width: 100%;
+  z-index: 1000;
+  background-color: rgba(0, 0, 0, 0.5) !important;
+  box-shadow: 0px 20px 20px 0px rgba(0, 0, 0, 0.3);
+  transition: .3s;
+}
+
+@media(max-width: 992px) {
+  body > .nav, body > .primary-color {
+    background-color: #151625 !important;
+  }
+}
+
+.homelogo {
+  width: 40%;
+  margin-bottom: -3vh;
+}
+
+@media(max-width: 992px) {
+  .homelogo {
+      width: 80%;
+      margin-bottom: -2vh;
+  }
+}
+</style>

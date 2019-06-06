ef21f5aaa3964030e14dfb5c4d02e381c0f98f20
James Harrison <james@fasttortoise.co.uk>
2019-06-06 22:42:09 +0100

    Added discord/merch/donate to .htaccess

--- src/routes.js
+++ src/routes.js
@@ -5,9 +5,6 @@ import Error from './views/Error'
 const routes = [
   { path: '/', component: Home },
   { path: '/staff', component: Staff },
-  { path: '/merch', beforeEnter() { location.href = 'https://shop.spreadshirt.co.uk/capecraft/all' } },
-  { path: '/donate', beforeEnter() { location.href = 'https://capecraftserver.buycraft.net/' } },
-  { path: '/discord', beforeEnter() { location.href = 'https://discord.gg/62MCajz' } },
   { path: '*', component: Error },
 ]
 

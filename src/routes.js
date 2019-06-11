58988622dab22ae82d4d994222011119a40fb65b
james090500 <james@fasttortoise.co.uk>
2019-06-11 16:43:34 +0100

    Work on the unban page

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import Home from './views/Home'
 import Rules from './views/Rules'
 import AltRules from './views/AltRules'
+import Unban from './views/Unban'
 import Staff from './views/Staff'
 import Error from './views/Error'
 
@@ -8,6 +9,7 @@ const routes = [
   { path: '/', component: Home },
   { path: '/rules', component: Rules },
   { path: '/rules/alts', component: AltRules },
+  { path: '/unban', component: Unban },
   { path: '/staff', component: Staff },
   { path: '*', component: Error },
 ]

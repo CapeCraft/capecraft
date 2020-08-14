a07ff816a962184e8fb160e0fe91e5b2cb26bfef
James Harrison <james@fasttortoise.co.uk>
2020-08-14 17:22:07 +0200

    Fixed logo Fixed Staff

---
+++ git.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+git pull
+rm /home/capecraft/src/View/Cache -R
+mkdir /home/capecraft/src/View/Cache
+chmod 777 /home/capecraft/src/View/Cache
--- src/Controllers/StaffController.php
+++ src/Controllers/StaffController.php
@@ -20,7 +20,6 @@
         'bf8b08a5714c46678f49efce56cb7dc5' => MojangAPI::getUsername('bf8b08a5714c46678f49efce56cb7dc5'),
         '58f1a83b425d4844823ffa04a201facd' => MojangAPI::getUsername('58f1a83b425d4844823ffa04a201facd'),
         '86ddbc979d9247f0a7ed0c1f328f0b11' => MojangAPI::getUsername('86ddbc979d9247f0a7ed0c1f328f0b11'),
-        '2d9a1bcaf032481e9da6c44f0cfab908' => MojangAPI::getUsername('2d9a1bcaf032481e9da6c44f0cfab908'),
         'c38ca4b68e3241e1a47850c1281e97c1' => MojangAPI::getUsername('c38ca4b68e3241e1a47850c1281e97c1')
       ];
 

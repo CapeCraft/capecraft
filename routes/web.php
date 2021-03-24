cf4fbf826434856b9d1510b9c7db0115d4e7d52e
James Harrison <james@fasttortoise.co.uk>
2021-03-24 08:43:26 +0000

    🌋 Fix vote links

--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,16 @@ Route::get('/donate', function() {
     return redirect('https://capecraft.buycraft.net');
 });
 
-Route::get('/mockdesign4', function() {
-    return redirect('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+Route::get('/vote1', function() {
+    return redirect('https://www.planetminecraft.com/server/capecraft-minecraft-survival-at-its-best/vote', 301);
+});
+
+Route::get('/vote2', function() {
+    return redirect('http://minecraft-mp.com/server/189099/vote/', 301);
+});
+
+Route::get('/vote3', function() {
+    return redirect('http://minecraftservers.org/vote/488669', 301);
 });
 
 Route::get('/{any}', function () {
